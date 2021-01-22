0c4d827c755ceeb58a8c415341ca462d89581a56
Abdelrahman Hegab <prenshegab@gmail.com>
2021-01-23 00:43:57 +0200

    Figured out the server js for socket io (Hopefully)

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const INDEX = '/index.html';
+const randomstring=require('randomstring');
+const path = require("path");
+//io.on('connection', () => { /* … */ });
+
+app.use(express.static(path.join(__dirname,"/")));
+
+if(process.env.PORT){
+	server.listen(process.env.PORT,function() {console.log('Server started')});
+}
+
+else{
+	server.listen(3000,function() {console.log('Server started on port 3000')});
+}
