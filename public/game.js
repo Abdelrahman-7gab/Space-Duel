c8342d2787c5b5729c3ae21c49d5a49cf2261ee1
Abdelrahman Hegab <prenshegab@gmail.com>
2021-01-25 06:14:07 +0200

    create and join game logic is working now

--- public/game.js
+++ public/game.js
@@ -1,13 +1,19 @@
 socket = io();
-var player1score = 0;
-var player2score = 0;
 var score1_span = document.getElementById("score1");
 var score2_span = document.getElementById("score2");
 var newGame_btn = document.getElementById("new");
 var joinGame_btn = document.getElementById("join");
 var RoomStatus_span =document.getElementById("RoomStatus");
 var Insert_div = document.getElementById("InsertDIV");
-var firstplayer = 0;
+var Room_sec = document.getElementById("RoomSec");
+var game_sec = document.getElementById("gameSec");
+var textBox = document.getElementById("box");
+var insertline = document.getElementById("insert");
+
+var player1score = 0;
+var player2score = 0;
+var player = 2;
+
 
 newGame_btn.addEventListener('click',function() {
     socket.emit("NewGame");
@@ -20,10 +26,41 @@ socket.on("RoomID",(data) =>{
     newGame_btn.style.display = "none";
     joinGame_btn.style.display = "none";
     Insert_div.style.display = "none";
-    firstplayer = 1; // the player who recieves the room code is always the first player
+    player = 1; // the player who recieves the room code is always the first player
 })
 
+function toGameSection(){
+    Insert_div.style.display = "none";
+    Room_sec.style.display = "none";
+    game_sec.style.display = "block";
+}
+
 joinGame_btn.addEventListener('click',function() {
-    
-  //io.sockets.adapter.get('roomId')
+    if(textBox.value == ""){
+        insertline.textContent = "Please insert code first"
+    }
+
+    else {
+        socket.emit("Join", textBox.value);
+    }
+
+  
 })
+
+socket.on("wrongCode",() =>{
+    if(player == 2)
+    insertline.textContent = "the code is not correct"
+})
+
+
+socket.on("FullRoom",() =>{
+    if(player == 2)
+    insertline.textContent = "This room is full";
+})
+
+socket.on("player2Joined",() =>{
+    socket.emit("the game is ready");
+})
+
+
+
--- server.js
+++ server.js
@@ -6,27 +6,39 @@ const INDEX = '/index.html';
 const randomstring=require('randomstring');
 const path = require("path");
 //io.on('connection', () => { /* … */ });
+var rooms = []
+var numberInrooms = []
 
 app.use(express.static(path.join(__dirname,"public")));
 
 io.on('connection', (socket) => {
 	console.log('Client connected');
-	socket.emit("player2Joined");
 	socket.on('disconnect',() => console.log('Client disconnected'));
-
-	socket.on("player2Joined",() =>{
-		socket.emit("test");
-	})
 
 	socket.on("NewGame",() =>{
 		const rand = randomstring.generate({length:4}).toLowerCase();
-		socket.join(rand)
+		socket.join(rand);
+		rooms.push(rand);
 		socket.emit("RoomID", rand);
 	})
 
 	socket.on("Join",(data) =>{
-		socket.join(data)
-		socket.emit("player2Joined");
+		//if(!io.sockets.adapter.get(data))
+		if(!rooms.includes(data))
+		{
+		console.log("wrong Code");
+		socket.emit("wrongCode");
+		}
+		else if (numberInrooms[rooms.indexOf(data)] == 2){
+			socket.emit("FullRoom");
+		}
+		else
+		{
+		console.log("JOINED");	
+		socket.join(data);
+		socket.to(data).emit("player2Joined");
+		numberInrooms[rooms.indexOf(data)] = 2;
+		}
 	})
 
 	
