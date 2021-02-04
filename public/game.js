9649e0efae1ff66077dcb85bb3928d1019222b7b
Abdelrahman Hegab <prenshegab@gmail.com>
2021-02-04 03:39:19 +0200

    reformatting code for more readability

--- public/game.js
+++ public/game.js
@@ -3,7 +3,7 @@ var score1_span = document.getElementById("score1");
 var score2_span = document.getElementById("score2");
 var newGame_btn = document.getElementById("new");
 var joinGame_btn = document.getElementById("join");
-var RoomStatus_span =document.getElementById("RoomStatus");
+var RoomStatus_span = document.getElementById("RoomStatus");
 var Insert_div = document.getElementById("InsertDIV");
 var Room_sec = document.getElementById("RoomSec");
 var game_sec = document.getElementById("gameSec");
@@ -45,63 +45,63 @@ var crowd = document.getElementById("crowd");
 
 
 var firstToGet = 3;
-var firstCHAR = "Astronaut"; 
-var secondCHAR = "Alien"; 
+var firstCHAR = "Astronaut";
+var secondCHAR = "Alien";
 var letsRematch = false;
 
-for(let i =0; i<rematchButtons.length;i++){
-    rematchButtons[i].addEventListener('click',function(){
+for (let i = 0; i < rematchButtons.length; i++) {
+    rematchButtons[i].addEventListener('click', function () {
 
         letsRematch = true;
 
-        socket.emit("rematchReq",roomID);
+        socket.emit("rematchReq", roomID);
 
-        for(let j =0;j<rematchMessages.length;j++){
-            if(isleft == false)
-            rematchMessages[j].textContent = "(Sent Rematch Request to opponent)"
+        for (let j = 0; j < rematchMessages.length; j++) {
+            if (isleft == false)
+                rematchMessages[j].textContent = "(Sent Rematch Request to opponent)"
             else
-            rematchMessages[j].textContent = "(Your opponent left the room)"
+                rematchMessages[j].textContent = "(Your opponent left the room)"
         }
     })
 }
 
 
-selectAstro.addEventListener('click',function(){
-         firstCHAR = "Astronaut";
-         secondCHAR = "Alien"; 
-         selectAstro.style.backgroundColor = "purple";
-         selectAlien.style.backgroundColor ="";
-    })
+selectAstro.addEventListener('click', function () {
+    firstCHAR = "Astronaut";
+    secondCHAR = "Alien";
+    selectAstro.style.backgroundColor = "purple";
+    selectAlien.style.backgroundColor = "";
+})
 
-selectAlien.addEventListener('click',function(){
-        firstCHAR = "Alien";
-        secondCHAR = "Astronaut"; 
-        selectAstro.style.backgroundColor = "";
-        selectAlien.style.backgroundColor ="purple";
-        })
+selectAlien.addEventListener('click', function () {
+    firstCHAR = "Alien";
+    secondCHAR = "Astronaut";
+    selectAstro.style.backgroundColor = "";
+    selectAlien.style.backgroundColor = "purple";
+})
 
-three.addEventListener('click',function(){
+three.addEventListener('click', function () {
     firstToGet = 3;
     goal.textContent = "Goal: 3";
     three.style.backgroundColor = "green";
     five.style.backgroundColor = "";
     ten.style.backgroundColor = "";
-    })
+})
 
-five.addEventListener('click',function(){
+five.addEventListener('click', function () {
     firstToGet = 5;
     goal.textContent = "Goal: 5";
     three.style.backgroundColor = "";
     five.style.backgroundColor = "green";
     ten.style.backgroundColor = "";
-    })
+})
 
-ten.addEventListener('click',function(){
-firstToGet = 10;
-goal.textContent = "Goal: 10";
-three.style.backgroundColor = "";
-five.style.backgroundColor = "";
-ten.style.backgroundColor = "green";
+ten.addEventListener('click', function () {
+    firstToGet = 10;
+    goal.textContent = "Goal: 10";
+    three.style.backgroundColor = "";
+    five.style.backgroundColor = "";
+    ten.style.backgroundColor = "green";
 })
 
 
@@ -112,47 +112,46 @@ var player = 2;
 
 
 audio.loop = true;
-sound.addEventListener('click',function() {
-    if(audio.paused){
-    audio.play();
-    sound.src ="Photos/soundOn.png"
-    }
-    else{
+sound.addEventListener('click', function () {
+    if (audio.paused) {
+        audio.play();
+        sound.src = "Photos/soundOn.png"
+    } else {
         audio.pause();
-        sound.src ="Photos/soundOff.png"
+        sound.src = "Photos/soundOff.png"
     }
-    
+
 })
 
-newGame_btn.addEventListener('click',function() {
+newGame_btn.addEventListener('click', function () {
     socket.emit("NewGame");
 
-    if(firstCHAR == "Alien"){
-    firstICON.src = "Photos/AlienHead.png"
-    secondICON.src = "Photos/AstroHead.png"
+    if (firstCHAR == "Alien") {
+        firstICON.src = "Photos/AlienHead.png"
+        secondICON.src = "Photos/AstroHead.png"
     }
 })
 
 var roomCreation = document.getElementById("RoomCreation");
 
-socket.on("RoomID",function(data){
+socket.on("RoomID", function (data) {
     //RoomStatus_span.setAttribute('style', 'white-space: pre;');
-    RoomStatus_span.textContent =('"waiting for a second player" \r\n Room Code:' + data );
-   // RoomStatus_span.style.display = "block";
-   roomCreation.style.display = "none";
+    RoomStatus_span.textContent = ('"waiting for a second player" \r\n Room Code:' + data);
+    // RoomStatus_span.style.display = "block";
+    roomCreation.style.display = "none";
     player = 1; // the player who recieves the room code is always the first player
-    if(firstCHAR == "Astronaut")
-    reposition();
+    if (firstCHAR == "Astronaut")
+        reposition();
 
 })
 
 
-function reposition(){
+function reposition() {
 
     //Bottom player becomes astronaut instead of alien and top becomes alien
 
     Paper_Pic.src = "Photos/Paper.png";
-    paperDown_anim.src ="Photos/Paper.png";
+    paperDown_anim.src = "Photos/Paper.png";
     paperUp_anim.src = "Photos/PaperAlien.png";
     Rock_Pic.src = "Photos/Rock.png";
     rockDown_anim.src = "Photos/Rock.png";
@@ -163,28 +162,26 @@ function reposition(){
 
 }
 
-joinGame_btn.addEventListener('click',function() {
-    if(textBox.value == ""){
+joinGame_btn.addEventListener('click', function () {
+    if (textBox.value == "") {
         insertline.textContent = "Please insert code first"
-    }
-
-    else {
+    } else {
         socket.emit("Join", textBox.value.toLowerCase());
     }
 })
 
-socket.on("wrongCode",function(){
-    if(player == 2)
-    insertline.textContent = "the code is not correct"
+socket.on("wrongCode", function () {
+    if (player == 2)
+        insertline.textContent = "the code is not correct"
 })
 
 
-socket.on("FullRoom",function(){
-    if(player == 2)
-    insertline.textContent = "This room is full";
+socket.on("FullRoom", function () {
+    if (player == 2)
+        insertline.textContent = "This room is full";
 })
 
-function toGameSection(){
+function toGameSection() {
     Insert_div.style.display = "none";
     Room_sec.style.display = "none";
     game_sec.style.display = "block";
@@ -193,50 +190,50 @@ function toGameSection(){
 var roomID;
 var select;
 
-socket.on("player2Joined",function(ID){
+socket.on("player2Joined", function (ID) {
     roomID = ID;
-        select = {
-        choice : "n",
-        room : ID
+    select = {
+        choice: "n",
+        room: ID
     }
 
 
-    if(player == 1){
-   let prefs = {
-        char: firstCHAR,
-        room : ID,
-        limit: firstToGet
-    }
-    socket.emit("userPrefs",prefs);
+    if (player == 1) {
+        let prefs = {
+            char: firstCHAR,
+            room: ID,
+            limit: firstToGet
+        }
+        socket.emit("userPrefs", prefs);
 
-}
+    }
     setTimeout(toGameSection, 500);
 })
 
-socket.on("changes",function(plot){
-    if(player == 2){
+socket.on("changes", function (plot) {
+    if (player == 2) {
 
-    firstCHAR = plot.character;
-    firstToGet = plot.num;
+        firstCHAR = plot.character;
+        firstToGet = plot.num;
 
-    if(plot.num === 3)
-    goal.textContent = "Goal: 3";
+        if (plot.num === 3)
+            goal.textContent = "Goal: 3";
 
-    if(plot.num === 5)
-    goal.textContent = "Goal: 5";
+        if (plot.num === 5)
+            goal.textContent = "Goal: 5";
 
-    if(plot.num === 10)
-    goal.textContent = "Goal: 10";
+        if (plot.num === 10)
+            goal.textContent = "Goal: 10";
 
-    if(firstCHAR == "Astronaut")
-    secondCHAR = "Alien";
+        if (firstCHAR == "Astronaut")
+            secondCHAR = "Alien";
 
-    if(firstCHAR == "Alien"){
-        secondCHAR ="Astronaut"
-    reposition();
-    firstICON.src = "Photos/AlienHead.png"
-    secondICON.src = "Photos/AstroHead.png"
-    }
+        if (firstCHAR == "Alien") {
+            secondCHAR = "Astronaut"
+            reposition();
+            firstICON.src = "Photos/AlienHead.png"
+            secondICON.src = "Photos/AstroHead.png"
+        }
 
     }
 })
@@ -244,137 +241,134 @@ socket.on("changes",function(plot){
 var firstChoice = "n"; //n for no choice.. changes to r / p / s
 var secondChoice = "n"
 
-Rock_Pic.addEventListener('click',function() {
+Rock_Pic.addEventListener('click', function () {
     select.choice = "r"
 
     socket.emit("PlayerChoice" + player, select);
 })
 
-Paper_Pic.addEventListener('click',function() {
+Paper_Pic.addEventListener('click', function () {
     select.choice = "p"
     socket.emit("PlayerChoice" + player, select);
 })
 
-Scissors_Pic.addEventListener('click',function() {
+Scissors_Pic.addEventListener('click', function () {
     select.choice = "s"
     socket.emit("PlayerChoice" + player, select);
 })
 
-socket.on("firstChoice",function(data){
+socket.on("firstChoice", function (data) {
     firstChoice = data;
-    if(secondChoice !== "n"){
-        getWinner(firstChoice,secondChoice);
-    }
-    else{
+    if (secondChoice !== "n") {
+        getWinner(firstChoice, secondChoice);
+    } else {
         StatusMessage();
     }
 })
 
 
-socket.on("secondChoice",function(data){
+socket.on("secondChoice", function (data) {
     secondChoice = data;
-    if(firstChoice !== "n"){
-        getWinner(firstChoice,secondChoice);
-    }
-    else{
+    if (firstChoice !== "n") {
+        getWinner(firstChoice, secondChoice);
+    } else {
         StatusMessage();
     }
 })
 
 var isleft = false;
 
-socket.on("opponentLeft",function(){
-    isleft =true;
-    status_div.textContent = "Your opponent has left the game"; 
-    for(let j =0;j<rematchMessages.length;j++){
+socket.on("opponentLeft", function () {
+    isleft = true;
+    status_div.textContent = "Your opponent has left the game";
+    for (let j = 0; j < rematchMessages.length; j++) {
         rematchMessages[j].textContent = "(Your opponent left the room)"
     }
 })
 
-socket.on("rematch?",function(){
+socket.on("rematch?", function () {
 
-    if(letsRematch == true)
-    socket.emit("ok",roomID);
+    if (letsRematch == true)
+        socket.emit("ok", roomID);
 
-    else{
-    for(let j =0;j<rematchMessages.length;j++){
-        rematchMessages[j].textContent = "(Your opponent wants a rematch)"
-    }
+    else {
+        for (let j = 0; j < rematchMessages.length; j++) {
+            rematchMessages[j].textContent = "(Your opponent wants a rematch)"
+        }
     }
 
 })
 
-socket.on("resetmatch",function(){
-    
+socket.on("resetmatch", function () {
+
     FirstPlayerScore = 0;
-    SecondPlayerScore =0;
+    SecondPlayerScore = 0;
     score1_span.textContent = 0;
-    score2_span.textContent =0;
-    alienWIN_div.style.display ="none";
-    astroWIN_div.style.display ="none";
-    gameContainer_div.style.display ="flex";
+    score2_span.textContent = 0;
+    alienWIN_div.style.display = "none";
+    astroWIN_div.style.display = "none";
+    gameContainer_div.style.display = "flex";
     status_div.textContent = "Make Your Move"
     crowd.textContent = "Rematch? why would I want to see that"
-    for(let j =0;j<rematchMessages.length;j++){
+    for (let j = 0; j < rematchMessages.length; j++) {
         rematchMessages[j].textContent = ""
     }
     letsRematch = false;
 })
 
 
-function getStatus(letter){
-    if(isleft == true)
-    return "Your opponent has left.";
-    else if(letter == "r")
-    return "You Selected ROCK, \r\nWaiting for opponent";
-    else if(letter == "p")
-    return "You Selected PAPER, \r\nWaiting for opponent";
-    else if(letter == "s")
-    return "You Selected Scissors, \r\nWaiting for opponent";
+function getStatus(letter) {
+    if (isleft == true)
+        return "Your opponent has left.";
+    else if (letter == "r")
+        return "You Selected ROCK, \r\nWaiting for opponent";
+    else if (letter == "p")
+        return "You Selected PAPER, \r\nWaiting for opponent";
+    else if (letter == "s")
+        return "You Selected Scissors, \r\nWaiting for opponent";
     else
-    return "Your Opponent has selected.";
+        return "Your Opponent has selected.";
 }
 
-function StatusMessage(){
-    if(player == 1)
-    status_div.textContent = getStatus(firstChoice);
+function StatusMessage() {
+    if (player == 1)
+        status_div.textContent = getStatus(firstChoice);
 
-    else{
+    else {
         status_div.textContent = getStatus(secondChoice);
     }
 }
 
-function getWinner(first,second){
+function getWinner(first, second) {
     let winner;
-    if(first+second == "rs" || first+second == "sp" || first+second =="pr")
-    winner = "first";
-    else if(first+second == "sr" || first+second == "ps" || first+second =="rp")
-    winner="second";
+    if (first + second == "rs" || first + second == "sp" || first + second == "pr")
+        winner = "first";
+    else if (first + second == "sr" || first + second == "ps" || first + second == "rp")
+        winner = "second";
     else
-    winner = "draw";
+        winner = "draw";
 
     updateValues(winner);
 }
 
-var winnerIsAstro = ["<span style ='color:purple;'> Astronaut </span> just REKT the Alien!","<span style ='color:purple'> Astronaut </span> confirms, this is EZ.","<span style ='color:purple'>ASTRO </span> BOY IS ON FIIIRE!","<span style ='color:purple'>NASA </span>is very proud","Let <span style ='color:purple'>Astro </span>go back to earth already."];
-var winnerIsAlien = ["YOU GOT <span style ='color:green'>ALIENISED </span>","<span style ='color:green'>Alien: </span> I don't believe in humans","Taste my <span style ='color:green'>green </span>victory","<span style ='color:green'> Alien: </span> &#8523&#8513&#8487&#8479&#8524&#8523&#8513&#8487&#8479&#8524","<span style ='color:green'>Alien: </span>haha Astro-NOT *nobody laughs*"];
-var DRAWLIST = ["<span style ='color:gray'>Crowd: *BOOOOOOOO*</span>","<span style ='color:gray'>DRAW? WHY ARE WE WATCHING THIS</span>","<span style ='color:gray'>This is Booring</span>","<span style ='color:gray'>Should've just watched netflix</span>","<span style ='color:gray'>DRAW? THEY BOTH SUCK</span>"];
+var winnerIsAstro = ["<span style ='color:purple;'> Astronaut </span> just REKT the Alien!", "<span style ='color:purple'> Astronaut </span> confirms, this is EZ.", "<span style ='color:purple'>ASTRO </span> BOY IS ON FIIIRE!", "<span style ='color:purple'>NASA </span>is very proud", "Let <span style ='color:purple'>Astro </span>go back to earth already."];
+var winnerIsAlien = ["YOU GOT <span style ='color:green'>ALIENISED </span>", "<span style ='color:green'>Alien: </span> I don't believe in humans", "Taste my <span style ='color:green'>green </span>victory", "<span style ='color:green'> Alien: </span> &#8523&#8513&#8487&#8479&#8524&#8523&#8513&#8487&#8479&#8524", "<span style ='color:green'>Alien: </span>haha Astro-NOT *nobody laughs*"];
+var DRAWLIST = ["<span style ='color:gray'>Crowd: *BOOOOOOOO*</span>", "<span style ='color:gray'>DRAW? WHY ARE WE WATCHING THIS</span>", "<span style ='color:gray'>This is Booring</span>", "<span style ='color:gray'>Should've just watched netflix</span>", "<span style ='color:gray'>DRAW? THEY BOTH SUCK</span>"];
 
 
-function winScreen(character){
-    if(character == "Alien")
-    alienWIN_div.style.display ="flex"; 
+function winScreen(character) {
+    if (character == "Alien")
+        alienWIN_div.style.display = "flex";
     else
-    astroWIN_div.style.display ="flex";
+        astroWIN_div.style.display = "flex";
 }
 
-function updateValues(data)
-{
+function updateValues(data) {
     let firstTemp = firstChoice;
     let secondTemp = secondChoice;
     var randomIndex = Math.floor(Math.random() * (5));
 
-    if(player == 2){
+    if (player == 2) {
         firstTemp = secondChoice;
         secondTemp = firstChoice;
     }
@@ -383,84 +377,73 @@ function updateValues(data)
     secondChoice = "n";
     status_div.textContent = "Make Your Move";
 
-    if(data == "first"){
+    if (data == "first") {
 
-        FirstPlayerScore +=1;
+        FirstPlayerScore += 1;
 
-        if(FirstPlayerScore == firstToGet){
-            gameContainer_div.display ="none"; 
-            setTimeout(winScreen, 3000,firstCHAR); 
+        if (FirstPlayerScore == firstToGet) {
+            gameContainer_div.display = "none";
+            setTimeout(winScreen, 3000, firstCHAR);
         }
         score1_span.textContent = FirstPlayerScore;
-            if(firstCHAR == "Astronaut")
+        if (firstCHAR == "Astronaut")
             crowd.innerHTML = winnerIsAstro[randomIndex];
-            else
+        else
             crowd.innerHTML = winnerIsAlien[randomIndex];
-        }
-    
-        else if(data == "second"){
-            SecondPlayerScore +=1;
-            if(SecondPlayerScore == firstToGet){
-                gameContainer_div.display ="none";
-                setTimeout(winScreen, 3000,secondCHAR);  
+    } else if (data == "second") {
+        SecondPlayerScore += 1;
+        if (SecondPlayerScore == firstToGet) {
+            gameContainer_div.display = "none";
+            setTimeout(winScreen, 3000, secondCHAR);
 
-            }
-            score2_span.textContent = SecondPlayerScore;
-            if(firstCHAR == "Astronaut")
+        }
+        score2_span.textContent = SecondPlayerScore;
+        if (firstCHAR == "Astronaut")
             crowd.innerHTML = winnerIsAlien[randomIndex];
-            else
-            crowd.innerHTML = winnerIsAstro[randomIndex];  
-        }
-        else{
-            crowd.innerHTML = DRAWLIST[randomIndex];  
-        }
+        else
+            crowd.innerHTML = winnerIsAstro[randomIndex];
+    } else {
+        crowd.innerHTML = DRAWLIST[randomIndex];
+    }
 
     gameContainer_div.style.display = "none";
 
-    
 
-    if(firstTemp == "r"){
+
+    if (firstTemp == "r") {
         rockDown_anim.classList.add("botAnim");
-         }
+    } else if (firstTemp == "p") {
+        paperDown_anim.classList.add("botAnim");
+    } else {
+        scissorsDown_anim.classList.add("botAnim");
+    }
 
-  else if(firstTemp =="p"){
-            paperDown_anim.classList.add("botAnim");
-        }
-
-    else{
-            scissorsDown_anim.classList.add("botAnim");
-        }
-
-    if(secondTemp == "r"){
-            rockUp_anim.classList.add("upAnim");
-         }
-
-    else if(secondTemp =="p"){
-              paperUp_anim.classList.add("upAnim");
-         }
-
-    else{
-               scissorsUp_anim.classList.add("upAnim");
-        }
+    if (secondTemp == "r") {
+        rockUp_anim.classList.add("upAnim");
+    } else if (secondTemp == "p") {
+        paperUp_anim.classList.add("upAnim");
+    } else {
+        scissorsUp_anim.classList.add("upAnim");
+    }
 
 
     setTimeout(hideAnimation, 3200);
- 
+
 }
-function hideAnimation(){
+
+function hideAnimation() {
     var botanimlist = document.getElementsByClassName("winAnimation");
     var upanimlist = document.getElementsByClassName("winAnimationUp");
 
-    for(let i = 0 ; i<botanimlist.length;i++){
+    for (let i = 0; i < botanimlist.length; i++) {
         botanimlist[i].classList.remove("botAnim")
     }
 
-    for(let j = 0 ; j<upanimlist.length;j++){
+    for (let j = 0; j < upanimlist.length; j++) {
         upanimlist[j].classList.remove("upAnim")
     }
 
 
-    if(FirstPlayerScore!=firstToGet&&SecondPlayerScore!=firstToGet)
-    gameContainer_div.style.display = "flex";   
+    if (FirstPlayerScore != firstToGet && SecondPlayerScore != firstToGet)
+        gameContainer_div.style.display = "flex";
 }
-
